fdc716316d22d4c930edbbf1d580a16b565d45c8
mattybigback <mattybigback@gmail.com>
2022-07-20 22:21:54 +0100

    Update flash.sh

--- flash.sh
+++ flash.sh
@@ -1,1 +1,1 @@
-avrdude -p t25 -c usbasp -B 16kHz -Uflash:w:ATTiny25_LP_Blink_PORTB.ino.hex:i -v
+avrdude -v -pattiny25 -cusbasp -e -Uefuse:w:0xFF:m -Uhfuse:w:0b11010111:m -Ulfuse:w:0xC4:m -Uflash:w:ATTiny25_LP_Blink_PORTB.ino.hex:i -B 16kHz
