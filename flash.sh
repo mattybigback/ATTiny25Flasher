2184531856c33f1efe8f4bab80dae818f837351a
mattybigback <mattybigback@gmail.com>
2022-07-20 22:24:30 +0100

    Update flash.sh

--- flash.sh
+++ flash.sh
@@ -1,1 +1,1 @@
-avrdude -v -pattiny25 -cusbasp -e -Uefuse:w:0xFF:m -Uhfuse:w:0b11010111:m -Ulfuse:w:0xC4:m -Uflash:w:ATTiny25_LP_Blink_PORTB.ino.hex:i -B 16kHz
+avrdude -v -pattiny25 -cusbasp -e -Uefuse:w:0xFF:m -Uhfuse:w:0b11010111:m -Ulfuse:w:0xC4:m -Uflash:w:ATTiny25_Flasher.hex:i -B 16kHz
